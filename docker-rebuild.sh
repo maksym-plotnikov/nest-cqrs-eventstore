afc79d1e93c426f02827941cffa1097c2e41ce30
maksym,plotnikov <maksym.plotnikov@lemberg.co.uk>
2021-02-02 10:31:18 +0200

    Bind Node version to v14.15.4, add Docker scripts

---
+++ docker-rebuild.sh
@@ -0,0 +1,29 @@
+. ~/.nvm/nvm.sh
+nvm use
+echo "Stopping all containers..."
+docker-compose down
+echo "Recreate and spin-up DEVELOPMENT environment"
+docker-compose up -d --force-recreate --build
+
+# Establish connection to database
+echo "Wait for Postgres connection..."
+# Stop execution if we have an error
+set -e
+# Same as in docker-compose.yml
+POSTGRES_HOST="127.0.0.1:5432"
+POSTGRES_USER="root"
+POSTGRES_PASSWORD="root"
+POSTGRES_DB="smplct_view_app"
+POSTGRES_CONTAINER_ID=$(docker ps | awk ' /postgres/ { print $1 }')
+echo "$POSTGRES_CONTAINER_ID"
+# Try to connect to PostgreSQL
+until docker exec -it $POSTGRES_CONTAINER_ID psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping..."
+  sleep 1
+done
+>&2 echo "Postgres is up - executing command"
+# Install / Update dependencies
+yarn
+yarn migration:run backend
+yarn seed:run backend
+yarn watch:dev
