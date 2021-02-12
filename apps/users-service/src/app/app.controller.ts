1d2b03d6730b0ad4b0651e524800dfcee139e102
maksym,plotnikov <maksym.plotnikov@lemberg.co.uk>
2021-02-12 09:40:04 +0200

    Lint affected command update amd lint fix

--- apps/users-service/src/app/app.controller.ts
+++ apps/users-service/src/app/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @MessagePattern({ cmd: 'ping' })
-    ping(_: any) {
+    ping() {
         return this.appService.respond();
     }
 }
